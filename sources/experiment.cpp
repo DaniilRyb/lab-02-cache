db41f4fc72d69a91e5e66e3b1d3cbbf864330fba
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2020-12-20 00:26:09 +0300

    update changes experiment.cpp

--- sources/experiment.cpp
+++ sources/experiment.cpp
@@ -1,11 +1,12 @@
 // Copyright 2020 Daniil Rybakov <pochatworld7@gmail.com>
 #include <experiment.hpp>
 const double half = 0.5;
+const int one = 1;
 const double oneAndHalf = 1.5;
 const size_t powTwo = 2;
 const int sixteen = 16;
 const int a = 4;
-const size_t hundred = 1000;
+const size_t thousand = 1000;
 experiment::experiment() {
   for (size_t counts = half * vectorCacheSize[0];
        counts <= oneAndHalf * vectorCacheSize[2]; counts *= powTwo) {
@@ -13,16 +14,15 @@ experiment::experiment() {
   }
 }
 std::vector<double> experiment::DirectExperiment() {
-  int* arr = nullptr;
   std::vector<double> vectorDuration;
   for (const auto& i : CountOfExperiments) {
-    arr = new int[static_cast<int>(i / a)];
+    int* arr = new int[static_cast<int>(i / a)];
     for (size_t j = 0; j < i / a; j += sixteen) {
       k = arr[j];
     }
     std::chrono::system_clock::time_point start =
         std::chrono::system_clock::now();
-    for (size_t it = 0; it < hundred; ++it) {
+    for (size_t it = 0; it < thousand; ++it) {
       for (size_t j = 0; j < i / a; j += sixteen) {
         k = arr[j];
       }
@@ -31,50 +31,48 @@ std::vector<double> experiment::DirectExperiment() {
         std::chrono::system_clock::now();
     std::chrono::duration<double> duration = end - start;
     vectorDuration.push_back(duration.count());
+    delete[] arr;
   }
-  delete[] arr;
   return vectorDuration;
 }
 std::vector<double> experiment::ReverseExperiment() {
-  int* arr = nullptr;
   std::vector<double> vectorDuration;
   for (const auto& i : CountOfExperiments) {
-    arr = new int[static_cast<int>(i / a)];
+    int* arr = new int[static_cast<int>(i / a)];
     for (size_t j = 0; j < i / a; j -= sixteen) {
-      k = arr[j - 1];
+      k = arr[j];
     }
     std::chrono::system_clock::time_point start =
         std::chrono::system_clock::now();
-    for (size_t it = 0; it < hundred; ++it) {
+    for (size_t it = 0; it < thousand; ++it) {
       for (size_t j = 0; j < i / a; j -= sixteen) {
-        k = arr[j - 1];
+        k = arr[j];
       }
     }
     std::chrono::system_clock::time_point end =
         std::chrono::system_clock::now();
     std::chrono::duration<double> duration = end - start;
     vectorDuration.push_back(duration.count());
+    delete[] arr;
   }
-  delete[] arr;
   return vectorDuration;
 }
 std::vector<double> experiment::RandomExperiment() {
-  int* arr = nullptr;
   std::vector<double> vectorDuration;
   std::vector<size_t> randomVectorOfIndex;
   for (const auto& i : CountOfExperiments) {
-    arr = new int[static_cast<int>(i / a)];
+    int* arr = new int[static_cast<int>(i / a)];
     for (size_t j = 0; j < i / a; j += sixteen) {
       k = arr[j];
       randomVectorOfIndex.push_back(j);
     }
-    std::random_device rd;
-    std::mt19937 generator(rd());
+    // std::random_device rd;
+    std::mt19937 generator(time(NULL));
     std::shuffle(randomVectorOfIndex.begin(), randomVectorOfIndex.end(),
                  generator);
     std::chrono::system_clock::time_point start =
         std::chrono::system_clock::now();
-    for (size_t count = 0; count < hundred; ++count) {
+    for (size_t count = 0; count < thousand; ++count) {
       for (const auto& index : randomVectorOfIndex) {
         k = arr[index];
       }
@@ -83,8 +81,8 @@ std::vector<double> experiment::RandomExperiment() {
         std::chrono::system_clock::now();
     std::chrono::duration<double> duration = end - start;
     vectorDuration.push_back(duration.count());
+    delete[] arr;
   }
-  delete[] arr;
   return vectorDuration;
 }
 size_t experiment::getCounter() { return CountOfExperiments.size(); }
@@ -95,12 +93,12 @@ std::ostream& operator<<(std::ostream& os, experiment& exp) {
   os << " experiments" << std::endl;
   os << " -experiment:" << std::endl;
   for (size_t j = 0; j < exp.getCounter(); ++j) {
-    os << "   number " << j + 1 << std::endl;
+    os << "   number " << j + one << std::endl;
     os << "   input_data:" << std::endl;
     os << "     buffer_size: " << exp.CountOfExperiments[j] / oneKByte << " kb"
        << std::endl;
     os << "   results:" << std::endl;
-    os << "     direction: " << exp.DirectExperiment()[j] * hundred << " ms"
+    os << "     direction: " << exp.DirectExperiment()[j] * thousand << " ms"
        << std::endl;
   }
   os << std::endl << std::endl;
@@ -109,12 +107,12 @@ std::ostream& operator<<(std::ostream& os, experiment& exp) {
   os << " experiments" << std::endl;
   os << " -experiment:" << std::endl;
   for (size_t j = 0; j < exp.getCounter(); ++j) {
-    os << "   number " << j + 1 << std::endl;
+    os << "   number " << j + one << std::endl;
     os << "   input_data:" << std::endl;
     os << "     buffer_size: " << exp.CountOfExperiments[j] / oneKByte << " kb"
        << std::endl;
     os << "   results:" << std::endl;
-    os << "     direction: " << exp.ReverseExperiment()[j] * hundred << " ms"
+    os << "     direction: " << exp.ReverseExperiment()[j] * thousand << " ms"
        << std::endl;
   }
   os << std::endl << std::endl;
@@ -123,12 +121,12 @@ std::ostream& operator<<(std::ostream& os, experiment& exp) {
   os << " experiments" << std::endl;
   os << " -experiment:" << std::endl;
   for (size_t j = 0; j < exp.getCounter(); ++j) {
-    os << "   number " << j + 1 << std::endl;
+    os << "   number " << j + one << std::endl;
     os << "   input_data:" << std::endl;
     os << "     buffer_size: " << exp.CountOfExperiments[j] / oneKByte << " kb"
        << std::endl;
     os << "   results:" << std::endl;
-    os << "     direction: " << exp.RandomExperiment()[j] * hundred << " ms"
+    os << "     direction: " << exp.RandomExperiment()[j] * thousand << " ms"
        << std::endl;
   }
   return os;
